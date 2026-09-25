c867c3b9a1d6be1e03754921ca21380af0911de0
harshalshewade1996-design <harshalshewade1996@gmail.com>
2026-09-25 22:14:29 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -4,3 +4,7 @@ window.SUPABASE_CONFIG = {
   url: 'https://oydzhtwpeoyfeuesyntd.supabase.co',
   anonKey: 'sb_publishable_4L_7OayPtSAAP4asmlhZXA_6StN3DTt'
 };
+const SUPABASE_URL = "https://oydzhtwpeoyfeuesyntd.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_4L_7OayPtSAAP4asmlhZXA_6StN3DTt";
